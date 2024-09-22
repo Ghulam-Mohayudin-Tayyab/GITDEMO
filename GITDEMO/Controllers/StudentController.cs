4b0006f82935381b3e600627ed5fd5ec6fac13d2
Ghulam Mohayudin Tayyab <tayyabchaudhry114@gmail.com>
2024-09-22 17:29:45 +0500

    Add project files.

---
+++ GITDEMO/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using GITDEMO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace GITDEMO.Controllers
+{
+    public class StudentController : Controller
+    {
+        // Hardcoded student list for simplicity (you can later use a database)
+        public static List<Student> students = new List<Student>();
+
+        // Action method to show form
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // Action method to handle form submission
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                students.Add(student);
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // Action method to display the list of students
+        public ActionResult Index()
+        {
+            return View(students);
+        }
+    }
+}
